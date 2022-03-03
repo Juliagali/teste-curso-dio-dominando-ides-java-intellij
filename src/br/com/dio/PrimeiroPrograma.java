3374ea10d4e10e9f06102688e5857ab9708bad80
Juliagali <juliagalibarroso@gmail.com>
2022-03-03 14:32:33 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.modal.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+       Gato gato = new Gato();
+        System.out.println(gato);
+
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("hello world!" + (a+b));*/
+
+    }
+}
