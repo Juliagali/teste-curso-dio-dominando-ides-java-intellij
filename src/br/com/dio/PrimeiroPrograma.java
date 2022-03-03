dca223a2927415170da00f0499073bdb725577a1
Juliagali <juliagalibarroso@gmail.com>
2022-03-03 15:24:40 -0300

    criando class Livro (erro)

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -8,9 +8,49 @@ public class PrimeiroPrograma {
         System.out.println(gato);
 
 
+        Object numPaginas;
+        Livro livro1 = new livro (nome "O problema dos 3 corpos", numPaginas;300);
+        System.out.println(livro1);
         /*int a = 5;
         int b = 3;
         System.out.println("hello world!" + (a+b));*/
 
     }
 }
+
+class livro {
+   private String nome;
+   private Integer numPaginas;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    public livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+}
+
